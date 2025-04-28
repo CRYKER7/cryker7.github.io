1d4118e463da983121661bfd24ae152f3ccaab83
CRYKER <78180693+CRYKER7@users.noreply.github.com>
2025-04-28 13:54:39 -0600

    Update and rename driver.php to driver.js

--- drivers/driver.js
+++ drivers/driver.js
@@ -1,4 +1,4 @@
-<?php
+/* <?php
     session_set_cookie_params(0);
     session_start();
     
@@ -21,4 +21,20 @@
                 include_once('./static/pages/main.php');
                 break;
     }
-?>
+?> */
+
+// static/js/importarComponentes.js
+
+function cargarComponente(id, ruta) {
+    fetch(ruta)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error(`Error al cargar ${ruta}:`, error));
+}
+
+// Carga los componentes
+cargarComponente("header", "components/header.html");
+cargarComponente("nav", "components/headMenu.html");
+cargarComponente("footer", "components/footer.html");
