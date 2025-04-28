b715eaadfaedba3acaaf08ed3d0b0d2bfa06eed2
CRYKER <78180693+CRYKER7@users.noreply.github.com>
2025-04-28 16:42:37 -0600

    save

--- static/js/router.js
+++ static/js/router.js
@@ -12,7 +12,7 @@ function cargarPeticion() {
     const contenedor = document.getElementById("contenido-principal");
 
     if (pet) {
-        fetch(`static/pages/${pet}.html`)
+        fetch(`pages/${pet}.html`)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Página no encontrada');
@@ -28,7 +28,7 @@ function cargarPeticion() {
             });
     } else {
         // Si no hay parámetro, puedes cargar una página por defecto
-        fetch(`static/pages/main.html`)
+        fetch(`pages/main.html`)
             .then(response => response.text())
             .then(html => {
                 contenedor.innerHTML = html;
