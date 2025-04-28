b3994aaabadcad38d7e65b2f23c5c3e3862cd67b
CRYKER <78180693+CRYKER7@users.noreply.github.com>
2025-04-28 15:11:30 -0600

    Inicio

--- drivers/driver.js
+++ drivers/driver.js
@@ -1,30 +1,3 @@
-/* <?php
-    session_set_cookie_params(0);
-    session_start();
-    
-    $pet = '';
-    
-    if (isset($_REQUEST['pet'])){ 
-        $pet = $_REQUEST['pet']; 
-    }
-    
-            
-    switch ($pet) {
-            case '':
-                include_once('./static/pages/main.php');
-                break;
-            case 'work':
-                include_once('./static/pages/work.php');
-                break;
-                
-            default:
-                include_once('./static/pages/main.php');
-                break;
-    }
-?> */
-
-// static/js/importarComponentes.js
-
 function cargarComponente(id, ruta) {
     fetch(ruta)
         .then(response => response.text())
@@ -35,6 +8,6 @@ function cargarComponente(id, ruta) {
 }
 
 // Carga los componentes
-cargarComponente("header", "components/header.html");
+cargarComponente("head", "components/head.html");
 cargarComponente("nav", "components/headMenu.html");
 cargarComponente("footer", "components/footer.html");
